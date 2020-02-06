37c696be33cc444d839af932efa3d426b23653e3
aaronbyrd86 <aaronbyrd86@gmail.com>
2020-02-05 19:45:39 -0600

    created functions

--- js/script.js
+++ js/script.js
@@ -16,9 +16,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
-
+const studentList = document.querySelector(".student-list").getElementsByTagName("li");
+const pageItems = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -34,7 +33,22 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
-
+function showPage(list, page)
+{
+   let startIndex = (page * pageItems) - pageItems;
+   let endIndex = page * pageItems;
+   console.log(`showPage() called with a page value of ${page}`);
+   for(let i = 0; i < list.length; i++)
+   {
+      if(i >= startIndex && i < endIndex)
+      {
+         list[i].style.display = "block";
+      }
+      else{
+         list[i].style.display = "none";
+      }
+   }
+}
 
 
 
@@ -42,9 +56,53 @@ FSJS project 2 - List Filter and Pagination
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+function appendPageLinks(list)
+{
+   const page = document.querySelector(".page");
+   const div = document.createElement('div');
+   const ul = document.createElement("ul");
+   let numLi = Math.floor(list.length / pageItems);
+   if((list.length / pageItems) % pageItems != 0)
+      numLi++;
+   //console.log(`create ${numLi} list items`)
+   div.className = "pagination";
+   div.appendChild(ul);
 
+   //append list items to div's ul
+   for(let i = 0; i < numLi; i++)
+   {
+      let li = document.createElement('li');
+      
+      li.innerHTML = `<a href="#">${i + 1}</a>`;
+      ul.appendChild(li);
+   }
+
+   const links = div.getElementsByTagName("a");
+   
+   ul.firstElementChild.className = "active";
+
+   for(let i = 0; i < links.length; i++)
+   {
+      links[i].addEventListener("click", (event) => {
+         //console.log(`you clicked on link ${links[i].textContent}`)
+
+         //remove active class from all links
+         for(let j = 0; i < links.length; j++)
+         {
+            links[i].className = "";
+         }
+         //add active class to clicked link
+         event.target.className = "active";
+         //call showPage()
+         showPage(studentList, links[i].textContent);
+      })
+   }
+
+   page.appendChild(div);
+}
 
 
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(studentList);
